d067df924c06980f298c02b133ab6f6ac0d0412b
Carolina Rangel <carolina.tercilia@gmail.com>
2026-03-27 11:48:35 -0300

    inicialização do servidor adicionada

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,15 @@ import express, { json } from "express";
 import cors from "cors";
 import db from "./database";
 
+const routes = require("./routes");
+
 const app = express();
 
 app.use(cors());
-app.use(json());
+app.use(express.json());
+
+app.use("/", routes);
+
+app.listen(3000, () => {
+    console.log("Servidor rodando em http://localhost:3000");
+});
