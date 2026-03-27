d7f6cbe2d76c337dbf97a1d70bf758985561ad2b
Carolina Rangel <carolina.tercilia@gmail.com>
2026-03-27 11:23:17 -0300

    criação de pacote e base de dados

---
+++ backend/database.js
@@ -0,0 +1,18 @@
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database("./database/livros.db");
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE IT NOT EXISTS livros (
+            id INTEDER PRIMARY KEY AUTOICREMENT,
+            titulo TEXT,
+            autor TEXT,
+            nota INTEGER,
+            favorito BOOLEAN,
+            data TEXT
+        )
+    `);
+});
+
+module.exports = db;
